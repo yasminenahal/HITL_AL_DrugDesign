30332d7747b938fdb71b73b1fedcbc13cb549caa
Yasmine Nahal <72565158+yasminenahal@users.noreply.github.com>
2023-06-06 15:07:21 +0200

    Update logPEvaluationModel

--- scripts/simulated_expert_delta.py
+++ scripts/simulated_expert_delta.py
@@ -1,37 +1,30 @@
 import pickle
 import numpy as np
-from utils import fingerprints_from_mol
-from rdkit.Chem.Crippen import MolLogP
+from utils import fingerprints_from_mol, double_sigmoid
+from tdc import Oracle
 
 class logPEvaluationModel():
     def __init__(self):
         pass
 
-    def calc_logp(self, mol):
-        if mol:
-            score = MolLogP(mol)
+    def oracle_score(self, smi):
+        oracle = Oracle(name = 'LogP')
+        if smi:
+            score = oracle(smi)
             return float(score)
         return 0.0
 
-    def feedback_score(self, mol, noise, reward, human_agreement_threshold):
-        if mol:
-            human_score = self.calc_logp(mol) + noise
-            delta = np.abs(human_score - reward)
-            print("reward :", reward, "human score :", human_score)
-            # if human disagrees
-            if delta > human_agreement_threshold:
-                return human_score
-            else:
-                return int(0)
+    def human_score(self, smi, noise):
+        if smi:
+            human_score = self.oracle_score(smi) + noise
+            print("human score :", human_score)
+            return float(human_score)
+        else:
+            return 0.0
 
-    def feedback_label(self, mol, noise, human_range):
-        if mol:
-            human_score = self.calc_logp(mol) + noise
-            # if human agrees
-            if human_score >= human_range[0] and human_score <= human_range[1]:
-                return 1
-            else:
-                return 0
+    def utility(self, x, low, high, alpha_1 = 10, alpha_2 = 10):
+        utility_score = double_sigmoid(x, low, high, alpha_1, alpha_2)
+        return utility_score
 
 class ActivityEvaluationModel():
     """Oracle scores based on an ECFP classifier for activity."""
