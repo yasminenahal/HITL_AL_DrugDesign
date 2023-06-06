1d5ea6432bba292c65136832c21401b621a102ab
Yasmine Nahal <72565158+yasminenahal@users.noreply.github.com>
2023-06-06 15:11:25 +0200

    Update RandomForestReg

--- bioactivity_models/RandomForest.py
+++ bioactivity_models/RandomForest.py
@@ -4,15 +4,20 @@ import numpy as np
 from scripts.acquisition import vote_entropy_qbc
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
-class RandomForestModel(RandomForestClassifier):
+class RandomForestClf(RandomForestClassifier):
     def __init__(self, fit):
-        super(RandomForestModel, self).__init__()
+        super(RandomForestClf, self).__init__()
         self.fit = fit
 
     def _reinitialize_regressor(self, x, y, sample_weight = None, save_to_path = None):
-        rf = RandomForestRegressor(n_estimators = 300)
+        rf = RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=None,
+                      max_features='auto', max_leaf_nodes=None,
+                      min_impurity_decrease=0.0, min_impurity_split=None,
+                      min_samples_leaf=1, min_samples_split=2,
+                      min_weight_fraction_leaf=0.0, n_estimators=50,
+                      n_jobs=None, oob_score=False, random_state=None,
+                      verbose=0, warm_start=False)
         rf.fit(x, y, sample_weight = sample_weight)
-
         if save_to_path is not None:
             pickle.dump(rf, open(save_to_path, 'wb'))
 
@@ -38,7 +43,23 @@ class RandomForestModel(RandomForestClassifier):
             )
         return vote_entropy
 
-    # likely to be removed
+class RandomForestReg(RandomForestRegressor):
+    def __init__(self, fit):
+        super(RandomForestReg, self).__init__()
+        self.fit = fit
+
+    def _reinitialize_regressor(self, x, y, sample_weight = None, save_to_path = None):
+        rf = RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=None,
+                      max_features='auto', max_leaf_nodes=None,
+                      min_impurity_decrease=0.0, min_impurity_split=None,
+                      min_samples_leaf=1, min_samples_split=2,
+                      min_weight_fraction_leaf=0.0, n_estimators=50,
+                      n_jobs=None, oob_score=False, random_state=None,
+                      verbose=0, warm_start=False)
+        rf.fit(x, y, sample_weight = sample_weight)
+        if save_to_path is not None:
+            pickle.dump(rf, open(save_to_path, 'wb'))
+
     def _uncertainty(self, x):
         individual_trees = self.fit.estimators_
         subEstimates = np.array(
