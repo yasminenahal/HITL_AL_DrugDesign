8afee519da8930755b0fa573fbf1dd7312ce6f35
Yasmine Nahal <72565158+yasminenahal@users.noreply.github.com>
2023-06-27 12:31:10 +0200

    Update RandomForest.py

--- bioactivity_models/RandomForest.py
+++ bioactivity_models/RandomForest.py
@@ -1,35 +1,37 @@
 import pickle
-
 import numpy as np
+
+from torch import tensor
 from scripts.acquisition import vote_entropy_qbc
+from scripts.epig import epig_from_probs
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
 class RandomForestClf(RandomForestClassifier):
-    def __init__(self, fit):
+    def __init__(self, fitted_model):
         super(RandomForestClf, self).__init__()
-        self.fit = fit
+        self.fitted_model = fitted_model
 
-    def _reinitialize_regressor(self, x, y, sample_weight = None, save_to_path = None):
-        rf = RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=None,
-                      max_features='auto', max_leaf_nodes=None,
-                      min_impurity_decrease=0.0, min_impurity_split=None,
-                      min_samples_leaf=1, min_samples_split=2,
-                      min_weight_fraction_leaf=0.0, n_estimators=50,
-                      n_jobs=None, oob_score=False, random_state=None,
-                      verbose=0, warm_start=False)
-        rf.fit(x, y, sample_weight = sample_weight)
-        if save_to_path is not None:
-            pickle.dump(rf, open(save_to_path, 'wb'))
+    def _estimators(self):
+        return self.fitted_model.estimators_
+    
+    def _predict_proba(self, x):
+        return self.fitted_model.predict_proba(x)
 
-    def _reinitialize_classifier(self, x, y, sample_weight = None, save_to_path = None):
+    def _retrain(self, x, y, sample_weight = None, save_to_path = None):
         rf = RandomForestClassifier(n_estimators = 300, class_weight={0: 1, 1: 2})
         rf.fit(x, y, sample_weight = sample_weight)
 
         if save_to_path is not None:
             pickle.dump(rf, open(save_to_path, 'wb'))
 
+    def _get_prob_distribution(self, x):
+        prob_dist = [estimator.predict_proba(x) for estimator in self._estimators()]
+        prob_dist = np.stack(prob_dist, axis = 1)
+        prob_dist = tensor(prob_dist)
+        return prob_dist
+
     def _qbc(self, x):
-        prop_votes = self.fit.predict_proba(x)
+        prop_votes = self.fitted_model.predict_proba(x)
         vote_entropy = []
 
         for i in range(len(prop_votes)):
@@ -43,12 +45,22 @@ class RandomForestClf(RandomForestClassifier):
             )
         return vote_entropy
 
-class RandomForestReg(RandomForestRegressor):
-    def __init__(self, fit):
-        super(RandomForestReg, self).__init__()
-        self.fit = fit
+    def _estimate_epig(self, prob_pool: tensor, prob_target: tensor):
+        return epig_from_probs(prob_pool, prob_target)
 
-    def _reinitialize_regressor(self, x, y, sample_weight = None, save_to_path = None):
+class RandomForestReg(RandomForestRegressor):
+    def __init__(self, fitted_model):
+        super(RandomForestReg, self).__init__()
+        self.fitted_model = fitted_model
+
+    def _estimators(self):
+        return self.fitted_model.estimators_
+    
+    def _predict(self, x):
+        return self.fitted_model.predict(x)
+
+    def _retrain(self, x, y, sample_weight = None, save_to_path = None):
+        # TODO: automatically read the parameters of the pickled model
         rf = RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=None,
                       max_features='auto', max_leaf_nodes=None,
                       min_impurity_decrease=0.0, min_impurity_split=None,
@@ -61,7 +73,7 @@ class RandomForestReg(RandomForestRegressor):
             pickle.dump(rf, open(save_to_path, 'wb'))
 
     def _uncertainty(self, x):
-        individual_trees = self.fit.estimators_
+        individual_trees = self.fitted_model.estimators_
         subEstimates = np.array(
             [
                 tree.predict(np.stack(x))
@@ -69,3 +81,12 @@ class RandomForestReg(RandomForestRegressor):
             ]
         )
         return np.std(subEstimates, axis=0)
+
+    def _get_prob_distribution(self, x):
+        prob_dist = [estimator.predict(x) for estimator in self._estimators()]
+        prob_dist = np.stack(prob_dist, axis = 1)
+        prob_dist = tensor(prob_dist)
+        return prob_dist
+
+    def _estimate_epig(self, prob_pool: tensor, prob_target: tensor):
+        return epig_from_probs(prob_pool, prob_target, classification = False)
