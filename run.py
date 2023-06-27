34f2e9993fde47bf05899cc1161664f86c843166
Yasmine Nahal <72565158+yasminenahal@users.noreply.github.com>
2023-06-27 12:10:53 +0200

    Add new arguments and automatically remove output dir if already exists

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 # load dependencies
+from json.encoder import INFINITY
 import sys
 import pickle
 import os
@@ -10,34 +11,18 @@ import rdkit.Chem as Chem
 from numpy.random import default_rng
 
 from utils import fingerprints_from_mol
-from scripts.simulated_expert_delta import ActivityEvaluationModel, logPEvaluationModel
-from scripts.write_classif_logp import write_REINVENT_config
+from scripts.simulated_expert import ActivityEvaluationModel, logPEvaluationModel
+from scripts.write_config import write_REINVENT_config
 from bioactivity_models.RandomForest import RandomForestReg
 from scripts.acquisition import select_query
 
-def do_run(seed, acquisition = None, sigma_noise = 0):
-    ##############################
-    # Quick options
-    FIT_MODEL = False # whether to fit a Stan model or not
-    LOAD_MODEL = True # load Stan model from disc
-    SUBSAMPLE = True # Reduce the size of the pool of unlabeled molecules to reduce computation time
-    ##############################
+def do_run(seed, dirname, init_model_path, K = 2, acquisition = None, sigma_noise = 0.0, T = 10, n_queries = 30):
     
-    jobid = f"demo_logpSmall_{acquisition}_noise{sigma_noise}"
+    jobid = f"{dirname}_{acquisition}_noise{sigma_noise}"
     jobname = "fine-tune predictive component"
 
     np.random.seed(seed)
     rng = default_rng(seed)
-
-    ########### HITL setup #################
-    T = 1 # number of HITL iterations
-    n = 30 # number of molecules shown to the simulated chemist at each iteration
-    #n0 = 20 # number of molecules shown to the expert at initialization
-    K = 10 # number of REINVENT runs: usage: K=2 for one HITL round (T*n queries); K=3 for two HITL rounds (2*(T*n) queries)
-    ########################################
-    
-    bioactivity_model = "toy_example_OracleBased"
-    initial_model_path = f"/home/klgx638/Projects/reinvent-hitl-calibration/logp_model/{bioactivity_model}.pkl"
 
     # change these path variables as required
     reinvent_dir = os.path.expanduser("/home/klgx638/Projects/reinventcli")
@@ -48,26 +33,41 @@ def do_run(seed, acquisition = None, sigma_noise = 0):
     conf_filename = "config.json"
 
     # create root output dir
-    if not os.path.exists(os.path.join(output_dir)):
-        os.makedirs(output_dir)
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
     
     print(f"Creating output directory: {output_dir}.")
     configuration_JSON_path = write_REINVENT_config(reinvent_dir, reinvent_env, output_dir, conf_filename, jobid, jobname)
     print(f"Creating config file: {configuration_JSON_path}.")
 
-    if acquisition:
-        # human interaction starts from the same first batch of generated compounds
-        initial_dir = f"/home/klgx638/Generations/HITL_qsar_experiments/demo_logpSmall_None_noise{sigma_noise}_seed{seed}"
-        # create an initialization folder
-        os.makedirs(os.path.join(output_dir, "iteration_0"))
-        # copy initialization scaffold memory in the initialization folder
-        shutil.copy(os.path.join(initial_dir, "results/scaffold_memory.csv"), os.path.join(output_dir, "iteration_0"))
-    
-    print(f"Running MPO experiment with K={K}, T={T}, n_queries={n}, seed={seed}. \n Results will be saved at {output_dir}")
+    # write initial model path in configuration
+    configuration = json.load(open(os.path.join(output_dir, conf_filename)))  
+    configuration_scoring_function = configuration["parameters"]["scoring_function"]["parameters"]
+    for i in range(len(configuration_scoring_function)):
+        if configuration_scoring_function[i]["component_type"] == "predictive_property":
+            configuration_scoring_function[i]["specific_parameters"]["model_path"] = init_model_path
 
-    # initialize human feedback model
-    feedback_model = logPEvaluationModel()
-    print("Loading feedback model.")
+    if acquisition:
+        # initialize the active learning with the same pool of generated compounds resulting from a standard Reinvent run
+        initial_dir = f"/home/klgx638/Generations/HITL_qsar_experiments/{dirname}_None_noise{sigma_noise}_seed{seed}"
+        if os.path.exists(initial_dir): # if you already have a standard Reinvent run
+            # copy the file containing the initial unlabelled pool in your current directory
+            os.makedirs(os.path.join(output_dir, "iteration_0"))
+            try:
+                initial_unlabelled_pool = os.path.join(initial_dir, "results/scaffold_memory.csv")
+                shutil.copy(initial_unlabelled_pool, os.path.join(output_dir, "iteration_0"))
+            # if this file does not exist, skip this step
+            except FileNotFoundError:
+                pass
+        else: # if you do not have a standard Reinvent run, skip this step
+            pass
+
+        print(f"Running MPO experiment with K={K}, T={T}, n_queries={n_queries}, seed={seed}. \n Results will be saved at {output_dir}")
+
+        # initialize human feedback model
+        feedback_model = logPEvaluationModel()
+        print("Loading feedback model.")
 
     # load background training data used to pre-train the predictive model
     print("Loading D0.")
@@ -81,10 +81,11 @@ def do_run(seed, acquisition = None, sigma_noise = 0):
     print("Labels : ", y_train.shape)
 
     # load the predictive model
-    model_load_path = output_dir + '/{}_iteration_0.pkl'.format(bioactivity_model)
+    predictive_model_name = init_model_path.split("/")[-1].split(".")[0]
+    model_load_path = output_dir + '/{}_iteration_0.pkl'.format(predictive_model_name)
     if not os.path.exists(model_load_path):
-        shutil.copy(initial_model_path, output_dir)
-    fitted_model = pickle.load(open(initial_model_path, 'rb'))
+        shutil.copy(init_model_path, output_dir)
+    fitted_model = pickle.load(open(init_model_path, 'rb'))
     print("Loading predictive model.")
 
     # store expert scores
@@ -94,11 +95,20 @@ def do_run(seed, acquisition = None, sigma_noise = 0):
 
     for REINVENT_iteration in np.arange(1,K+1):
 
-        if REINVENT_iteration == 1 and acquisition: # skip REINVENT run and start from generated compounds in initial dir
-            with open(os.path.join(output_dir, "iteration_0/scaffold_memory.csv"), 'r') as file:
-                data = pd.read_csv(file)
-            data = data[data["Step"] < 100]
-            data.reset_index(inplace=True)
+        if REINVENT_iteration == 1 and acquisition:
+            if os.path.exists(os.path.join(output_dir, "iteration_0/scaffold_memory.csv")):
+                # start from your pre-existing pool of unlabelled compounds
+                with open(os.path.join(output_dir, "iteration_0/scaffold_memory.csv"), 'r') as file:
+                    data = pd.read_csv(file)
+                data = data[data["Step"] < 100]
+                data.reset_index(inplace=True)
+            else:
+                # generate a pool of unlabelled compounds with REINVENT
+                print("Run REINVENT")
+                os.system(reinvent_env + '/bin/python ' + reinvent_dir + '/input.py ' + configuration_JSON_path + '&> ' + output_dir + '/run.err')
+                
+                with open(os.path.join(output_dir, "results/scaffold_memory.csv"), 'r') as file:
+                    data = pd.read_csv(file)
 
         else:
             if(not READ_ONLY):
@@ -111,7 +121,7 @@ def do_run(seed, acquisition = None, sigma_noise = 0):
 
             with open(os.path.join(output_dir, "results/scaffold_memory.csv"), 'r') as file:
                 data = pd.read_csv(file)
-            
+        
         N = len(data)
         colnames = list(data) 
         smiles = data['SMILES']
@@ -154,24 +164,24 @@ def do_run(seed, acquisition = None, sigma_noise = 0):
             for t in np.arange(T): # T number of HITL iterations
                 print(f"iteration k={REINVENT_iteration}, t={t}")
                 # query selection
-                fit = RandomForestReg(fitted_model) # create function to load predictive model
-                if len(smiles) > n:
-                    new_query = select_query(N, n, list(smiles), fit, selected_feedback, acquisition, rng) # select n smiles with AL
+                model = RandomForestReg(fitted_model)
+                if len(smiles) > n_queries:
+                    new_query = select_query(data, n_queries, list(smiles), model, selected_feedback, acquisition, rng) # select n smiles with AL
                 else:
-                    new_query = select_query(N, len(smiles), list(smiles), fit, selected_feedback, acquisition, rng)
+                    new_query = select_query(data, len(smiles), list(smiles), model, selected_feedback, acquisition, rng)
                 # Initialize the expert values vector
-                s_bioactivity = np.zeros(len(new_query))
-                v_bioactivity = np.zeros(len(new_query))
+                s_bioactivity = []
+                v_bioactivity = []
                 # Sample expert noise
                 noise_sample = np.random.normal(mu, sigma, 1).item()
                 # Get expert feedback on selected queries
                 print(new_query)
-                for i in np.arange(len(new_query)):
-                    print(list(smiles)[i])
-                    #cur_mol = Chem.MolFromSmiles(list(smiles)[i])
-                    cur_mol = list(smiles)[i]
-                    v_bioactivity[i] = feedback_model.human_score(cur_mol, noise_sample)
-                    s_bioactivity[i] = feedback_model.utility(v_bioactivity[i], low = 2, high = 4)
+                for i in new_query:
+                    cur_mol = data.iloc[i]["SMILES"]
+                    print(cur_mol)
+                    value = feedback_model.human_score(cur_mol, noise_sample)
+                    v_bioactivity.append(value)
+                    s_bioactivity.append(feedback_model.utility(value, low = 2, high = 4))
                 
                 # Get raw scores and transformed score (if any) from the high scoring molecules in U
                 raw_scoring_component_names = ["raw_"+name for name in scoring_component_names] 
@@ -179,7 +189,7 @@ def do_run(seed, acquisition = None, sigma_noise = 0):
                 x =  data[scoring_component_names].to_numpy()
 
                 # get (binary) simulated chemist's responses
-                new_y = v_bioactivity
+                new_y = np.array(v_bioactivity)
                 accepted = [1 if s > 0.5 else 0 for s in s_bioactivity]
                 n_accept += [sum(accepted)]
 
@@ -191,24 +201,28 @@ def do_run(seed, acquisition = None, sigma_noise = 0):
                 # append feedback
                 if len(new_y) > 0:
                     selected_feedback = np.hstack((selected_feedback, new_query))
-                    y = np.concatenate([y, new_y])
 
                 mask = np.ones(N, dtype=bool)
                 mask[selected_feedback] = False
 
-            # use the augmented training data to retrain the model
-            new_mols = [Chem.MolFromSmiles(s) for s in data.iloc[selected_feedback].SMILES.tolist()]
-            new_x = fingerprints_from_mol(new_mols, type = "counts")
-            new_human_sample_weight = np.array([s if s > 0.5 else 1-s for s in s_bioactivity])
-            human_sample_weight = np.concatenate([human_sample_weight, new_human_sample_weight])
-            sample_weight = np.concatenate([sample_weight, human_sample_weight])
-            x_train = np.concatenate([x_train, new_x])
-            y_train = np.concatenate([y_train, y])
-            print(f"Augmented train set size at iteration {REINVENT_iteration}: {x_train.shape[0]} {y_train.shape[0]}")
+                # use the augmented training data to retrain the model
+                new_mols = [Chem.MolFromSmiles(s) for s in data.iloc[new_query].SMILES.tolist()]
+                new_x = fingerprints_from_mol(new_mols, type = "counts")
+                new_human_sample_weight = np.array([s if s > 0.5 else 1-s for s in s_bioactivity])
+                sample_weight = np.concatenate([sample_weight, new_human_sample_weight])
+                print(len(new_x), len(new_y))
+                x_train = np.concatenate([x_train, new_x])
+                y_train = np.concatenate([y_train, new_y])
+                print(f"Augmented train set size at iteration {REINVENT_iteration}: {x_train.shape[0]} {y_train.shape[0]}")
+                # save augmented training data
+                D_r = pd.DataFrame(np.concatenate([x_train, y_train.reshape(-1,1)], 1))
+                D_r.columns = [f"bit{i}" for i in range(x_train.shape[1])] + ["target"]
+                D_r.to_csv(os.path.join(output_dir, f"augmented_train_set_iter{REINVENT_iteration}.csv"))
 
-            # re-fit and save the model using the augmented train set and save to new directory
-            model_new_savefile = output_dir + '/{}_iteration_{}.pkl'.format(bioactivity_model, REINVENT_iteration)
-            fit._reinitialize_regressor(x_train, y_train, sample_weight = sample_weight, save_to_path = model_new_savefile)   
+                # re-fit and save the model using the augmented train set and save to new directory
+                model_new_savefile = output_dir + '/{}_iteration_{}.pkl'.format(predictive_model_name, REINVENT_iteration)
+                model._retrain(x_train, y_train, sample_weight = sample_weight, save_to_path = model_new_savefile)
+                fitted_model = pickle.load(open(model_new_savefile, 'rb'))
 
             # get current configuration
             configuration = json.load(open(os.path.join(output_dir, conf_filename)))
@@ -254,11 +268,17 @@ def do_run(seed, acquisition = None, sigma_noise = 0):
     print("Mean expert score : ", m_score)
 
 if __name__ == "__main__":
+    # TODO: add flag arguments 
     print(sys.argv)
     seed = int(sys.argv[1])
-    if len(sys.argv) > 2:
-        acquisition = sys.argv[2] # acquisition: 'uncertainty', 'random', 'thompson', 'greedy' (if None run with no human interaction)
-        sigma_noise = int(sys.argv[3])
-        do_run(seed, acquisition, sigma_noise)
+    dirname = str(sys.argv[2])
+    init_model_path = str(sys.argv[3])
+    K = int(sys.argv[4]) # number of REINVENT runs
+    if len(sys.argv) > 5:
+        acquisition = str(sys.argv[5]) # acquisition: 'uncertainty', 'random', 'thompson', 'greedy' (if None run with no human interaction)
+        sigma_noise = float(sys.argv[6])
+        T = int(sys.argv[7]) # number of HITL iterations
+        n_queries = int(sys.argv[8]) # number of molecules shown to the simulated chemist at each iteration
+        do_run(seed, dirname, init_model_path, K, acquisition, sigma_noise, T, n_queries)
     else:
-        do_run(seed)
+        do_run(seed, dirname, init_model_path, K)
