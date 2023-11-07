78f664f2a21bfe3cdd98ba08401689f26af5bb4e
Yasmine Nahal <72565158+yasminenahal@users.noreply.github.com>
2023-11-07 11:31:12 +0100

    Update RandomForest.py

--- models/RandomForest.py
+++ models/RandomForest.py
@@ -18,7 +18,8 @@ class RandomForestClf(RandomForestClassifier):
         return self.fitted_model.predict_proba(x)
 
     def _retrain(self, x, y, sample_weight = None, save_to_path = None):
-        rf = RandomForestClassifier(n_estimators = 300, class_weight={0: 1, 1: 2})
+        init_params = self.fitted_model.get_params()
+        rf = RandomForestClassifier(**init_params)
         rf.fit(x, y, sample_weight = sample_weight)
 
         if save_to_path is not None:
@@ -60,14 +61,8 @@ class RandomForestReg(RandomForestRegressor):
         return self.fitted_model.predict(x)
 
     def _retrain(self, x, y, sample_weight = None, save_to_path = None):
-        # TODO: automatically read the parameters of the pickled model
-        rf = RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=None,
-                      max_features='auto', max_leaf_nodes=None,
-                      min_impurity_decrease=0.0, min_impurity_split=None,
-                      min_samples_leaf=1, min_samples_split=2,
-                      min_weight_fraction_leaf=0.0, n_estimators=50,
-                      n_jobs=None, oob_score=False, random_state=None,
-                      verbose=0, warm_start=False)
+        init_params = self.fitted_model.get_params()
+        rf = RandomForestRegressor(**init_params)
         rf.fit(x, y, sample_weight = sample_weight)
         if save_to_path is not None:
             pickle.dump(rf, open(save_to_path, 'wb'))
